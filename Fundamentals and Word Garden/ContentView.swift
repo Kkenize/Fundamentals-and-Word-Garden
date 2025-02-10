f80b803ef424440e77932822c5a5addb67b707e0
Kkenize <Kkenizez@gmail.com>
2025-02-09 23:15:40 -0500

    Pushing Fundamentals, homework for week four. More updates for this homewok is to come.

--- Fundamentals and Word Garden/ContentView.swift
+++ Fundamentals and Word Garden/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Fundamentals and Word Garden
+//  Fundamentals Two
 //
 //  Created by Zhejun Zhang on 2/9/25.
 //
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            //        This is for setting background colors
+            //        ZStack {
+            //            Color.skyBlue
+            //                .ignoresSafeArea(edges: .all)
+            
+            VStack {
+                Text("You have skills!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(.white)
+                    .padding()
+//                    .frame(width: .infinity)
+                    .background(.skyBlue)
+                    .clipShape(RoundedRectangle(cornerRadius: 40))
+                
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                //                .frame(width: 300, height: 150)
+                //                    .border(.orange, width: 2)
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                Divider()
+                    .background(.gray)
+                    .frame(width: geometry.size.width, height: 1)
+                
+                
+                HStack {
+                    Button("Awesome") {
+                        messageString = "You are awesome!"
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        messageString = "You are great!"
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .padding()
+                
+// Sets the bottom safe area to skyBlue
+                Rectangle()
+                    .frame(height: 0)
+                    .background(.skyBlue)
+                
+            }
+            .padding()
+            .background(Gradient(colors: [.skyBlue, .green]))
+            //        }
         }
-        .padding()
     }
 }
 
