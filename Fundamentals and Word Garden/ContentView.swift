2ff4f1317378c5d5f09310d0c7dcd42b7af30354
Kkenize <Kkenizez@gmail.com>
2025-02-10 11:14:01 -0500

    First update of my word guarden app! I've kept the fundamentals in the app as well, under annotations.

--- Fundamentals and Word Garden/ContentView.swift
+++ Fundamentals and Word Garden/ContentView.swift
@@ -79,73 +79,153 @@
 //}
 
 //Below are contents for Fundamentals Two Way Bindings
+//import SwiftUI
+//
+//struct ContentView: View {
+//    @State private var count = 0
+//    @State private var toggleIsOn = true
+//    @State private var textEntered = ""
+//    @State private var colorSelected: Color = .red
+//    @State private var dateSelected = Date()
+//    @State private var stepperNumber = 1
+//    @State private var sliderNumber = 50.0
+//    
+//    var body: some View {
+//        VStack {
+//            
+//            Spacer()
+//            Spacer()
+//            
+//            Text("Press Count: \(count)")
+//                .font(.title2)
+//            
+//            Button("Add One") {
+//                count += 1
+//            }
+//            .buttonStyle(.borderedProminent)
+//            .font(.title2)
+//            
+//            Spacer()
+//            
+//            TextField("Enter a name:", text: $textEntered)
+//                .textFieldStyle(.roundedBorder)
+//            Text("Name Entered: \(textEntered)")
+//            
+//            Spacer()
+//            
+//            Toggle("Toggle is \(toggleIsOn ? "on" : "off"):", isOn: $toggleIsOn)
+//            
+//            Spacer()
+//            
+//            Rectangle()
+//                .fill(colorSelected)
+//                .frame(width: .infinity, height: 10)
+//            ColorPicker("Please select a color:", selection: $colorSelected)
+//            
+//            Spacer()
+//            
+//            DatePicker("Please select a date:", selection: $dateSelected)
+//            Text("Selected date is: \(dateSelected.formatted(date: .abbreviated, time: .shortened))")
+//            
+//            Spacer()
+//            
+//            Stepper("Stepper value: \(stepperNumber)", value: $stepperNumber, in: 1...10)
+//            
+//            Spacer()
+//            
+//            Slider(value: $sliderNumber, in: 0...100) {
+//                //Accessibility Code
+//            } minimumValueLabel: {
+//                Image(systemName: "speaker.minus")
+//                Text("0")
+//            } maximumValueLabel: {
+//                Image(systemName: "speaker.plus")
+//                Text("100")
+//            }
+//            Text("Slider value is: \(String(format:"%.1f", sliderNumber))%")
+//            
+//            Spacer()
+//        }
+//    }
+//}
+
+//Below are contents for the word garden app
 import SwiftUI
 
 struct ContentView: View {
-    @State private var count = 0
-    @State private var toggleIsOn = true
-    @State private var textEntered = ""
-    @State private var colorSelected: Color = .red
-    @State private var dateSelected = Date()
-    @State private var stepperNumber = 1
-    @State private var sliderNumber = 50.0
+    @State private var wordsGuessed = 0
+    @State private var wordsMissed = 0
+    @State private var wordsToGuess = ["SWIFT","DOG","CAT"]
+    @State private var gameMessage = "How Many Guesses To Cover The Hidden Words?"
+    @State private var currentWord = 0
+    @State private var guessedLetter = ""
+    @State private var imageName = "flower8"
+    @State private var playAgainHidden = true
+    @FocusState private var textFieldFocus: Bool
     
     var body: some View {
         VStack {
-            
-            Spacer()
-            Spacer()
-            
-            Text("Press Count: \(count)")
-                .font(.title2)
-            
-            Button("Add One") {
-                count += 1
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Words Guessed: \(wordsGuessed)")
+                    Text("Words Missed: \(wordsMissed)")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Words to Guess: \(wordsToGuess.count - wordsGuessed - wordsMissed)")
+                    Text("Words in Game: \(wordsToGuess.count)")
+                }
             }
-            .buttonStyle(.borderedProminent)
-            .font(.title2)
-            
+            .padding(.horizontal)
             Spacer()
+            Text(gameMessage)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding()
             
-            TextField("Enter a name:", text: $textEntered)
-                .textFieldStyle(.roundedBorder)
-            Text("Name Entered: \(textEntered)")
-            
-            Spacer()
-            
-            Toggle("Toggle is \(toggleIsOn ? "on" : "off"):", isOn: $toggleIsOn)
-            
-            Spacer()
-            
-            Rectangle()
-                .fill(colorSelected)
-                .frame(width: .infinity, height: 10)
-            ColorPicker("Please select a color:", selection: $colorSelected)
-            
-            Spacer()
-            
-            DatePicker("Please select a date:", selection: $dateSelected)
-            Text("Selected date is: \(dateSelected.formatted(date: .abbreviated, time: .shortened))")
-            
-            Spacer()
-            
-            Stepper("Stepper value: \(stepperNumber)", value: $stepperNumber, in: 1...10)
-            
-            Spacer()
-            
-            Slider(value: $sliderNumber, in: 0...100) {
-                //Accessibility Code
-            } minimumValueLabel: {
-                Image(systemName: "speaker.minus")
-                Text("0")
-            } maximumValueLabel: {
-                Image(systemName: "speaker.plus")
-                Text("100")
+            Text("_ _ _ _ _")
+                .font(.title)
+            if playAgainHidden {
+                HStack {
+                    TextField("", text: $guessedLetter)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: 30)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.gray, lineWidth: 2)
+                        }
+                        .keyboardType(.asciiCapable)
+                        .submitLabel(.done )
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.characters)
+                        .onChange(of: guessedLetter) {
+                            guessedLetter = guessedLetter.trimmingCharacters(in: .letters.inverted)
+                            guard let lastChar = guessedLetter.last else { return }
+                            guessedLetter = String(lastChar).uppercased()
+                        }
+                        .focused($textFieldFocus)
+                    Button("Guess a letter!") {
+                        textFieldFocus = false
+                    }
+                    .buttonStyle(.bordered)
+                    .tint(.mint)
+                    .disabled(guessedLetter.isEmpty)
+                }
+            } else {
+                Button("Another Word?") {
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
             }
-            Text("Slider value is: \(String(format:"%.1f", sliderNumber))%")
-            
             Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
         }
+        .ignoresSafeArea(edges:.bottom)
+        
     }
 }
 
