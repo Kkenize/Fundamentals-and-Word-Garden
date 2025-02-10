14b2c06cca18e1e3094e742a0260db78067acbc1
Kkenize <Kkenizez@gmail.com>
2025-02-09 21:53:13 -0500

    Initial Commit

---
+++ Fundamentals and Word Garden/Fundamentals_and_Word_GardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fundamentals_and_Word_GardenApp.swift
+//  Fundamentals and Word Garden
+//
+//  Created by Zhejun Zhang on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Fundamentals_and_Word_GardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
